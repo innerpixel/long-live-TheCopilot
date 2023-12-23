3bd9310a2ac5d7cf9b7c188276f0f02cd8ba8ac2
innerpixel <ned.gabrielc@gmail.com>
2023-12-23 23:14:11 +0200

    Rainbow colors .. and trails ..

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight; 
 canvas.style.border = '1px solid white';
 const particlesArray = [];
+let hue = 0;
+
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -12,69 +14,81 @@ window.addEventListener('resize', function(){
 
 
 const mouse = { 
-    x: 0,
-    y: 0,
+    x: undefined,
+    y: undefined,
 };
 
 canvas.addEventListener('mousemove', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
+    for(let i = 0; i < 3; i++) {   
+        particlesArray.push(new Particle());
+   }
 })  
+
 canvas.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;  
+    for(let i = 0; i < 3; i++) {   
+         particlesArray.push(new Particle());
+    }
+    
+
 })
 
 class Particle{ 
     constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = mouse.x;
+        this.y = mouse.y;
+
+        // this.x = Math.random() * canvas.width;
+        // this.y = Math.random() * canvas.height;
         
-        this.size = Math.random() * 50 + 1;  
+        this.size = Math.random() * 10 + 1;  
         this.speedX = Math.random() * 3 - 1.5 ;
         this.speedY = Math.random() * 3 - 1.5 ;
         
         const colors = ["blue", "red", "green", "yellow", "orange"]; // Array of colors
-        this.color = colors[Math.floor(Math.random() * colors.length)]; // Random color from the array
+        this.color = 'hsl(' + hue + ', 100%, 50%)'; 
+         // this.color = colors[Math.floor(Math.random() * colors.length)]; // Random color from the array
     }
 
 
     draw(){
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = this.color ; // 'hsl(' + hue + ', 100%, 50%)';
         ctx.strokeStyle = 'white';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); 
+        ctx.arc( this.x, this.y, this.size, 0, Math.PI * 2); 
         ctx.fill();
     }
 
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
-        this.draw();
+        if(this.size > 0.2) this.size -= 0.1;
+        
+        // this.draw();
     }
 }
-
-
-function init(n){
-    for(let i = 0; i < n; i++){
-        particlesArray.push(new Particle());
-    }
-}
-
-init(100); 
-
-console.log(particlesArray);
 
 function handleParticles(){
     for(let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
-        particlesArray[i].draw();   
+        particlesArray[i].draw();  
+        if(particlesArray[i].size <= 0.2){
+            particlesArray.splice(i, 1);
+            i--;
+        } 
 
     }
 }
 
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.02)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    hue+=0.5;
+
     handleParticles();
     requestAnimationFrame(animate);
 }   
